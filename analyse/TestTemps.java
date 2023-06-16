c400932bc56a8b4555368ab0713851f9aca55719
[BIZETjln] <126144836+BIZETjln@users.noreply.github.com>
2023-06-16 21:13:36 +0200

    modification readme version final

--- analyse/TestTemps.java
+++ analyse/TestTemps.java
@@ -2,7 +2,7 @@ package exercice;
 
 import java.util.*;
 
-public class Main {
+public class TestTemps {
     public static void main(String[] args) {
         String texte = "exemple de texte exemplaire exemples a zoing classer wario 672 exemplaires exemplairem xylophone mignon ae at";
         List<Character> ordre = List.of('c', 't', 'e', '6', 'x', 'a', 'm', 'p', 'l', 's', 'r', 'd');
